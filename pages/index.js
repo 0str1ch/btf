6ae012f63ac60d61447e11ed8ab54a6ab7858377
0str1ch <get@smithed.xyz>
2018-12-14 22:25:35 -0500

    test without layout

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,10 @@
 import Container from "../components/Container";
-import Layout from "../components/Layout";
 import React from "react";
 
 export default () => (
-  <Layout>
     <Container padding>
       <section className="intro">
         <h1>BTF</h1>
       </section>
     </Container>
-  </Layout>
 );
